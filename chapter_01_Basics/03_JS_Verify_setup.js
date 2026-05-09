bbb41e5bf7405a61af65f8269604f713b9172dcc
PVSRaviKumar <ravikumarpallapothu@gmail.com>
2026-05-09 20:09:16 +0530

    Initial commit of Playwright learning code

---
+++ chapter_01_Basics/03_JS_Verify_setup.js
@@ -0,0 +1,8 @@
+console.log(process.platform);
+
+//MAC - Darwin
+// Windows - win32
+//linux - linux
+
+console.log(process.arch);
+console.log("node version" + process.version);
---
+++ chapter_02_JavaScript_Concepts/05_JS_Basics.js
@@ -0,0 +1,8 @@
+var v =10;
+console.log(v);
+
+name = "Ravi";
+console.log(name);
+
+var let=34;
+console.log(let);
